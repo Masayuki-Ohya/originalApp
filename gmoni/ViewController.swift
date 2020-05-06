1f03bd37d7677b74c4795cf7acbfdd864be59ed4
Masayuki-Ohya <oymsyk@gmail.com>
2020-05-06 13:24:47 +0900

    Initial Commit

---
+++ gmoni/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gmoni
+//
+//  Created by 大矢政行 on 2020/05/06.
+//  Copyright © 2020 masayuki.ohya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
